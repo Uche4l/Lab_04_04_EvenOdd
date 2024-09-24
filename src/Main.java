ebaaa9d2c49a3c69e9a747e972b48aa62f511841
samue <mbasu@mail.uc.edu>
2024-09-24 13:55:21 -0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("input the number below to see if it is even or odd");
+        int numToExamine = 2;
+
+        System.out.println("the number entered is even.");
+    }
+}
